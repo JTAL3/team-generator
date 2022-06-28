b0770de33aecbada106de914db55f5df80a78ce1
Jason Talbert <jason.talbert.jt@gmail.com>
2022-06-27 22:10:29 -0400

    file structure assigned

---
+++ tests/Employee.test.js
@@ -0,0 +1,17 @@
+// Employee is Parent class of
+//other 3 will extend Employee (Engineer, Intern, Manager)
+
+//Employee class will have following properties
+name
+
+id
+
+email
+
+getName()
+
+getId()
+
+getEmail()
+
+getRole() // Returns 'Employee'
---
+++ tests/Manager.test.js
@@ -0,0 +1,5 @@
+// in addition to Employees properties and methods, Manager will also have:
+
+officeNumber
+
+getRole() // Overridden to return 'Manager'
